cfb4add6b804104f165cc13cd8a8deef57c582ae
Benjamin King <ben@Benjamins-MacBook-Pro.local>
2021-03-25 21:27:05 +0000

    Made some changes to attempt re-upload if upload fails.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 import boto3
 import threading
 import os
+import time
 
 #The time (in seconds) in which a new recording file should be created
 splitTime = 1800
@@ -20,25 +21,32 @@ s3 = boto3.client("s3", region_name=regionName, endpoint_url=endpointUrl, aws_ac
 
 #Upload to s3 function
 def uploader(fileName):
+    print("TID: " + str(threading.get_ident()) + " " + fileName + " " + "Upload function called: " + fileName)
     with open(fileName, 'rb') as data:
         try:
             s3.upload_fileobj(data, bucketName, fileName)
         except:
-            print("Error on upload, not deleting local file.")
+            print(str(threading.get_ident()) + " " + fileName + " " + "Error on upload, not deleting: " + fileName)
+            # Recursively start another upload attempt
+            print(str(threading.get_ident()) + " " + fileName + " " + "Sleeping 300s before trying again... ")
+            time.sleep(300)
+            print(str(threading.get_ident()) + " " + fileName + " " + "Running recursively on: " + fileName)
+            uploader(fileName)
             return False
     # File uploaded successfully so local file deleted
+    print(str(threading.get_ident()) + " " + fileName + " " + "Upload sucessful: " + fileName)
     os.remove(fileName)
     return True
 
 
 with picamera.PiCamera() as camera:
-    camera.led = False
+    camera.led = False  
     camera.resolution = (640, 480)
 
     #Start initial recording
     date_time = datetime.datetime.now()
-    nameStamp = (date_time.strftime("%Y-%b-%d_%H:%M") + '.h264') 
-    print(nameStamp)
+    nameStamp = (date_time.strftime("%Y-%b-%d_%H:%M") + '.h264')
+    print(str(threading.get_ident()) + " " + nameStamp + " " + "Starting recording: " + nameStamp)
     camera.start_recording(nameStamp)
     camera.wait_recording(splitTime)
 
@@ -50,7 +58,6 @@ with picamera.PiCamera() as camera:
         #Roll over into new file
         date_time = datetime.datetime.now()
         nameStamp = (date_time.strftime("%Y-%b-%d_%H:%M") + '.h264')
-        print(nameStamp)
+        print(str(threading.get_ident()) + " " + nameStamp + " " + "Rolling over to: " + nameStamp)
         camera.split_recording(nameStamp)
         camera.wait_recording(splitTime)
-
